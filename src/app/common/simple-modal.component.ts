d748ee6f25b50e391cbb1c2b56d46c1af9cb95fa
GraciaDufort <graciadufort@gmail.com>
2020-11-15 23:05:11 -0500

    End of section 12

--- src/app/common/simple-modal.component.ts
+++ src/app/common/simple-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from "@angular/core";
+import { Component, ElementRef, Inject, Input, ViewChild } from "@angular/core";
+import { $ } from 'protractor';
+import { JQ_TOKEN } from './jQuery.service';
 
 @Component({
     selector: 'simple-modal',
@@ -10,4 +12,15 @@ import { Component, Input } from "@angular/core";
 export class SimpleModalComponent {
     @Input() title: string
     @Input() elementId: string
+    @Input() closeOnBodyClick: string
+    @ViewChild('modalContainer') containerRef: ElementRef
+
+    constructor(@Inject(JQ_TOKEN) private $: any) {
+
+    }
+
+    closeModal() {
+        if(this.closeOnBodyClick.toLocaleLowerCase()==="true")
+            this.$(this.containerRef.nativeElement).modal('hide')
+    }
 }
